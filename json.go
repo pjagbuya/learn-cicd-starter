593835b4a0ba6aae81874be323e5083d64e572a0
pjagbuya <pjagbuya2@gmail.com>
2025-04-30 19:38:40 +0800

    Fixed security issues

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err2 := w.Write(dat)
+
+	if err2 != nil {
+		log.Printf("Error writing response: %v", err2)
+	}
 }
